24390715a1ac7fdbce969f7695183af3279456d2
zqt <zqt20010102@gmail.com>
2024-02-26 21:54:33 +0800

    channel

---
+++ channelTX/main.go
@@ -0,0 +1,15 @@
+package main
+
+
+func main() {
+	ch := make(chan struct{})
+	go func() {
+		fmt.Println("start working")
+		time.Sleep(time.Second * 1)
+		ch <- struct{}{}
+	}()
+
+	<- ch
+
+	fmt.Println("finished")
+}
